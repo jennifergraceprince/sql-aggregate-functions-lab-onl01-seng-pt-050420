af2d0d858af5e10bdbdd38be55967fe0186b82dc
Jennifer Prince <jennifergraceprince@gmail.com>
2020-07-01 05:41:13 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -13,8 +13,9 @@ def average_student_gpa
 end
 
 def total_tardies_for_all_students
-  SELECT COUNT()
+  SELECT COUNT(tardies) FROM students AS total_tardies_for_all_students
 end
 
 def average_gpa_for_9th_grade
+  
 end
